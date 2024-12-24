edecfd1bd76788b191a5af09e2f65fad88f5ab01
zenghao <1054092535@qq.com>
2024-12-25 01:58:07 +0800

    feat✨: 添加ui组件库

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/CoolBI/',
+  plugins: [react(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+      editor: 'code',
+    }),
+  ],
+  resolve:{
+    alias:{
+      "@":'/src'
+    }
+  }
 })
