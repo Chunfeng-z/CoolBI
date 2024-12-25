6095c6079b9be7019d8dd63528496b06a48c679b
zenghao <1054092535@qq.com>
2024-12-25 22:07:54 +0800

    feat(pro): :sparkles: 增加emoji

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -73,7 +73,7 @@ module.exports = {
                 emoji: ':hammer:'
             }
         ],
-        useEmoji: false,
+        useEmoji: true,
         emojiAlign: 'center',
         useAI: false,
         //option...
