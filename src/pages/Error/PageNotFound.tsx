67527e57f99edeb465cbbf39dcb9436d19b7b631
zenghao <1054092535@qq.com>
2024-12-27 03:20:13 +0800

    feat(login): :sparkles: login 页面的开发

--- src/pages/Error/PageNotFound.tsx
+++ src/pages/Error/PageNotFound.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button, Result } from 'antd';
 
 const PageNotFound: React.FC = () => {
+
     return (
         <Result
             status="404"
             title="404"
-            subTitle="Sorry, the page you visited does not exist."
+            subTitle="抱歉，您访问的页面不存在。"
             extra={<Button type="primary">Back Home</Button>}
         />
     );
