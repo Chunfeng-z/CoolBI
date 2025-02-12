84ed1206b2fbee97de85413be39e97badf0c6e68
ZengHao <zenghao@ZengHaodeMacBook-Pro.local>
2025-02-12 09:59:57 +0800

    feat: :sparkles: 图表组件展示区域

--- src/pages/DashBoard/DashBoardPage.tsx
+++ src/pages/DashBoard/DashBoardPage.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-
+import LeftChartMenu from "../../components/dashboard/LeftChartMenu";
+const prefixCls = "dashboard-page";
 /** 创建、编辑仪表板界面,独立于home页面（独立的功能） */
 const DashBoardPage: React.FC = () => {
-  return <div>DashBoardPage</div>;
+  return (
+    <div className={`${prefixCls}-container`} style={{ display: "flex" }}>
+      <LeftChartMenu />
+      DashBoardPage
+    </div>
+  );
 };
 
 export default DashBoardPage;
