4f0830008c2f0b0703d25b478e8f1a27dd35bd9c
ZengHao <zenghao@ZengHaodeMacBook-Pro.local>
2025-02-12 23:52:25 +0800

    feat: :sparkles: 编写左侧图表菜单

--- src/components/dashboard/LeftChartMenu/ChartCategory/index.tsx
+++ src/components/dashboard/LeftChartMenu/ChartCategory/index.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import "./index.scss";
-const prefixCls = "chart-category-items";
+import { Tooltip } from "antd";
+const prefixCls = "chart-category";
+
+/** 图表项的数据类型 */
+type ChartItem = {
+  /** 图表项的名称 */
+  name: string;
+  /** 图表项的icon */
+  icon: string;
+  /** 图表的简介 */
+  description: string;
+};
+
+/** 图表分类属性 */
+interface IChartCategoryProps {
+  /** 图表分类名称 */
+  name: string;
+  /** 图表项列表 */
+  ChartItemsData: ChartItem[];
+}
 /** 图表分类 */
-const ChartCategory: React.FC = () => {
+const ChartCategory: React.FC<IChartCategoryProps> = (props) => {
+  const { name, ChartItemsData } = props;
   return (
-    <div
-      className={`${prefixCls}-container`}
-      style={{ display: "flex", flexWrap: "wrap", gap: 4 }}
-    >
-      {Array.from({ length: 10 }, (_, i) => (
-        <div
-          className="chart-items-container"
-          key={i}
-          style={{
-            padding: 10,
-            backgroundColor: "#EEEEEE",
-            borderRadius: 5,
-          }}
-        >
-          <div className="chart-items-icon">这是icon</div>
-          <div className="chart-items-name">name</div>
-        </div>
-      ))}
+    <div className={`${prefixCls}-container`}>
+      <span className={`${prefixCls}-title`}>{name}</span>
+      <div className={`${prefixCls}-items-container`}>
+        {ChartItemsData.map((item, index) => {
+          const { name, icon } = item;
+          return (
+            <Tooltip title={name} key={index}>
+              <div className="chart-items">
+                <span className="chart-items-icon">{icon}</span>
+                <span className="chart-items-name">{name}</span>
+              </div>
+            </Tooltip>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/dashboard/LeftChartMenu/LeftChartMenu.tsx
+++ src/components/dashboard/LeftChartMenu/LeftChartMenu.tsx
@@ -1,27 +1,47 @@
 import React from "react";
 import ChartCategory from "./ChartCategory";
 const prefixCls = "left-chart-menu";
+
+const chartItemsData = [
+  { name: "折线图", icon: "icon1", description: "折线图" },
+  { name: "柱状图", icon: "icon2", description: "柱状图" },
+  { name: "饼图", icon: "icon3", description: "饼图" },
+  { name: "散点图", icon: "icon4", description: "散点图" },
+  { name: "雷达图", icon: "icon5", description: "雷达图" },
+];
+
+type ChartItem = {
+  name: string;
+  icon: string;
+  description: string;
+};
 /** 仪表板左侧的图表菜单 */
 const LeftChartMenu: React.FC = () => {
+  /** 当前hover选中的图表 */
+  const [currentHoverChart, setCurrentHoverChart] = React.useState<ChartItem>({
+    name: "11",
+    icon: "222",
+    description: "xxxxx",
+  });
   return (
     <div className={`${prefixCls}-scroll-wrapper`}>
       <div className={`${prefixCls}-container`}>
         <div className={`${prefixCls}-title`}>{"图表组件"}</div>
         <div className={`${prefixCls}-body`}>
-          <div className="chart-category" style={{ margin: "10px 0" }}>
-            <span className="chart-category-title">{"线/面图"}</span>
-            <div
-              className="chart-category-body"
-              style={{
-                marginTop: 5,
-                width: "100%",
-                backgroundColor: "lightblue",
-              }}
-            >
-              <ChartCategory />
+          <div className="chart-category">
+            <div className="chart-category-body">
+              <ChartCategory name="11" ChartItemsData={chartItemsData} />
             </div>
           </div>
         </div>
+        {currentHoverChart && (
+          <div className={`${prefixCls}-description`}>
+            <span>{currentHoverChart.name}</span>
+            <span className="description-detail">
+              {currentHoverChart.description}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
