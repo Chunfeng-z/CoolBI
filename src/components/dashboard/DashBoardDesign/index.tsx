7d84ee208c96fc466832c57ae92fe3d6b098e21f
Chunfeng-z <1054092535@qq.com>
2025-02-21 00:20:22 +0800

    feat: :sparkles: 接入图表库

--- src/components/dashboard/DashBoardDesign/index.tsx
+++ src/components/dashboard/DashBoardDesign/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./index.scss";
+import CoolBarChart from "../../common/Charts/CoolBarChart";
 const prefixCls = "dashboard-design";
 /** 仪表板设计组件 */
 const DashBoardDesign = () => {
-  return <div className={`${prefixCls}-container`}>DashBoardDesign</div>;
+  return (
+    <div
+      className={`${prefixCls}-container`}
+      //   style={{ width: "200px", height: "200px" }}
+    >
+      <CoolBarChart />
+    </div>
+  );
 };
 
 export default DashBoardDesign;
