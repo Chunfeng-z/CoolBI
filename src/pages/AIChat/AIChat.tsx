aaece59aac5b913ab6c8f24a7828be51fd8d0e41
Chunfeng-z <1054092535@qq.com>
2025-03-08 21:25:33 +0800

    feat: :sparkles: 新增ai对话配置

--- src/pages/AIChat/AIChat.tsx
+++ src/pages/AIChat/AIChat.tsx
@@ -1,55 +1,177 @@
 import React, { useEffect, useState } from "react";
-import {
-  // 消息气泡
-  Bubble,
-  // 发送框
-  Sender,
-} from "@ant-design/x";
+import { Bubble, BubbleProps, Sender, Welcome } from "@ant-design/x";
 import "./index.scss";
-import { message } from "antd";
+import { GetRef, message } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { RolesType } from "@ant-design/x/es/bubble/BubbleList";
+import { Typography } from "antd";
+import markdownit from "markdown-it";
+const markdown = `### GPT的发展历程`;
+const md = markdownit({ html: true, breaks: true });
+
+const renderMarkdown: BubbleProps["messageRender"] = (content) => (
+  <Typography>
+    <div dangerouslySetInnerHTML={{ __html: md.render(content.trim()) }} />
+  </Typography>
+);
+
+const rolesAsFunction: RolesType = (bubbleData) => {
+  switch (bubbleData.role) {
+    case "ai":
+      return {
+        placement: "start" as const,
+        avatar: { icon: <UserOutlined />, style: { background: "#fde3cf" } },
+        shape: "corner",
+        // typing: { step: 2, interval: 50 },
+        messageRender: renderMarkdown,
+      };
+    case "user":
+      return {
+        placement: "end" as const,
+        avatar: { icon: <UserOutlined />, style: { background: "#87d068" } },
+        shape: "corner",
+      };
+    default:
+      return {};
+  }
+};
 
 const AIChat: React.FC = () => {
-  const [value, setValue] = useState<string>("Hello? this is X!");
+  const [sendValue, setSendValue] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [events, setEvents] = useState<string>("");
   const [messageApi, contextHolder] = message.useMessage();
+  const listRef = React.useRef<GetRef<typeof Bubble.List>>(null);
+  const [messageList, setMessageList] = useState<
+    {
+      key: number;
+      role: "ai" | "user";
+      content: string;
+    }[]
+  >([
+    {
+      key: 0,
+      role: "ai",
+      content: `
+> Render as markdown content to show rich text!
 
+Link: [Ant Design X](https://x.ant.design)
+`.trim(),
+    },
+  ]);
+  async function fetchData(sendValue: string) {
+    try {
+      const response = await fetch("http://localhost:3000/ds/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          model: "deepseek-r1",
+          messages: [{ role: "user", content: sendValue }],
+          stream: true,
+        }),
+      });
+
+      const reader = response.body!.getReader();
+      const decoder = new TextDecoder();
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        console.log(decoder.decode(value, { stream: true }));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const getDataSse = () => {
+    const eventSource = new EventSource("http://localhost:3000/sse");
+    let receivedText = ``;
+    eventSource.onmessage = (event) => {
+      receivedText += event.data;
+      console.log("receivedText", receivedText);
+
+      setMessageList((prev) => {
+        return [
+          ...prev,
+          { key: prev.length, role: "ai", content: receivedText },
+        ];
+      });
+    };
+    eventSource.onerror = (error) => {
+      console.error(error);
+      eventSource.close();
+      setLoading(false);
+    };
+  };
+  const [renderKey, setRenderKey] = React.useState(0);
   useEffect(() => {
     if (loading) {
-      const eventSource = new EventSource("http://localhost:3000/sse");
-
-      eventSource.onmessage = function (event) {
-        const data = JSON.parse(event.data);
-        console.log("EventSource received:", data);
-        setEvents((prev) => prev + `Current Time: ${data.time}`); // 添加新事件
-      };
-
-      eventSource.onerror = () => {
-        eventSource.close();
-        setLoading(false);
-      };
-
-      return () => {
-        eventSource.close();
-      };
+      // fetchData(sendValue);
+      getDataSse();
     }
   }, [loading]);
+  React.useEffect(() => {
+    const id = setTimeout(() => {
+      setRenderKey((prev) => prev + 1);
+    }, markdown.length * 100 + 2000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [renderKey]);
+
   return (
     <>
       {contextHolder}
-      <div className="ai-container">
-        <div className="ai-content" style={{ width: 500, marginTop: 100 }}>
-          <Bubble content="Hello, Ant Design X!" placement="start" role="ai" />
-          {events && <Bubble content={events} placement="start" role="ai" />}
+      <div className="chat-page-container">
+        <div className="message-container">
+          <Welcome
+            style={{
+              backgroundImage:
+                "linear-gradient(97deg, #f2f9fe 0%, #f7f3ff 100%)",
+              borderStartStartRadius: 4,
+            }}
+            icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
+            title="Hello, I'm DS"
+            description="Fine DS test"
+          />
+          {/* <Bubble.List
+            ref={listRef}
+            style={{ maxHeight: 800, padding: 10 }}
+            roles={rolesAsFunction}
+            items={messageList}
+          /> */}
+          <div className="x" key={renderKey}>
+            {/* {loading && ( */}
+            <Bubble
+              role="ai"
+              content={markdown}
+              typing
+              placement="start"
+              shape="corner"
+              messageRender={renderMarkdown}
+              avatar={{
+                icon: <UserOutlined />,
+                style: { background: "#fde3cf" },
+              }}
+            />
+            {/* )} */}
+          </div>
+        </div>
+        <div className="send-container">
           <Sender
-            value={value}
+            value={sendValue}
             loading={loading}
+            placeholder="请输入提问内容"
             onChange={(v) => {
-              setValue(v);
+              setSendValue(v);
             }}
             onSubmit={() => {
-              setValue("");
+              setSendValue("");
               setLoading(true);
+              setMessageList((prev) => [
+                ...prev,
+                { key: prev.length, role: "user", content: sendValue },
+              ]);
               messageApi.info("Send message!");
             }}
             onCancel={() => {
